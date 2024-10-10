7958cdeee47c9ae6bdeb51ea93d7bc7af4785fa4
leit07001 <leit07001@E-PW05C37S>
2024-10-10 12:59:03 +0200

    Add project files.

---
+++ Uppgift23/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace uppggift23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hej, välkommen till bil uthyrning.");
+                Console.WriteLine("Hur många dagar vill du hyra bilen?");
+                string antaldagar = Console.ReadLine();
+                int intdagar = int.Parse(antaldagar);
+                Console.WriteLine("Hur många kilometer kommer du att köra?");
+                string antalkm = Console.ReadLine();
+                double doublekm = double.Parse(antalkm);
+                double summa = 300 + (1 * doublekm) + ((intdagar - 1)) * 500;
+                Console.WriteLine("Det kommer att kosta " + summa + "kr" + " att hyra bilen.");
+            }
+            catch
+            {
+                Console.WriteLine("Använd endast siffror och kommatecken (inte punkt.)");
+            }
+        }
+    }
+}
